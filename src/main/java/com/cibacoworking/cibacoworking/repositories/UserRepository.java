347a3b600b2782861f45558b4c850abd8a0e74d3
ShailaGonzalez <shailagonzalezcarrero@gmail.com>
2024-09-23 11:47:44 +0200

    resolving problems

--- src/main/java/com/cibacoworking/cibacoworking/repositories/UserRepository.java
+++ src/main/java/com/cibacoworking/cibacoworking/repositories/UserRepository.java
@@ -1,10 +1,11 @@
 package com.cibacoworking.cibacoworking.repositories;
 
-
+import java.util.Optional;
 import com.cibacoworking.cibacoworking.models.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email); 
 }
